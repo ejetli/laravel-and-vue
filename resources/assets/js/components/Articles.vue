374e8ed66e52eb799633a8edd6b1a09b3c02e67f
etah jet <etah4077@gmail.com>
2019-04-26 23:23:13 +0100

    vue in laravel

---
+++ resources/assets/js/components/Articles.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="continal">
+    <h2>Articles</h2>
+    <form @submit.prevent="addArticle" class="md-3">
+      <div class="form-group">
+        <input type="text" name="" class="form-control" placeholder="Title" v-model="article.title">
+      </div>
+      <div class="form-group">
+        <textarea class="form-control" placeholder="Body" v-model="article.body"></textarea>
+      </div>
+      <div class="row">
+        <div class="col-md-3 offset-md-3">
+         <button type="submit" class="btn btn-info btn-block">Save</button>
+        </div>
+      </div>
+    </form>
+
+    <div class="card card-body md-2" v-for="article in articles" v-bind:key="article.id">
+      <h3>{{ article.title }}</h3>
+      <p>{{ article.body }}</p><br>
+      <hr>
+      <div class="row">
+        <div class="col-md-6">
+          <button @click="editArticle(article)" class="btn btn-warning md-2">Edit</button>
+        
+        
+          <button type="button" @click="deleteArticle(article.id)" class="btn btn-danger">Delete</button>
+        </div>
+      </div>
+      
+    </div><br><br>
+  <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li v-bind:class="[{disabled: !pagination.prev_page_url}]" class="page-item"><a class="page-link" href="#" 
+          @click="fetchArticles(pagination.prev_page_url)"
+          >Previous</a></li>
+        
+            <li class="page-item disabled"><a class="page-link text-dark" href="#">Page {{ pagination.current_page }} of {{ pagination.last_page }}</a></li>
+
+
+        <li v-bind:class="[{disabled: !pagination.next_page_url}]" class="page-item"><a class="page-link" href="#"
+          @click="fetchArticles(pagination.next_page_url)"
+          >Next</a></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        articles: [],
+        article: {
+          id: '',
+          title: '',
+          body: ''
+        },
+        article_id: '',
+        pagination: {},
+        edit: false
+      }
+    },
+
+    created() {
+      this.fetchArticles();
+    },
+
+    methods: {
+      fetchArticles(page_url) {
+        let vm = this;
+        page_url = page_url || '/api/articles'
+        fetch(page_url)
+          .then(res => res.json())
+          .then(res => {
+            this.articles = res.data;
+            vm.makePagination(res.meta, res.links);
+            console.log(res.data);
+          })
+          .catch(err => console.log(err)); 
+      },
+      makePagination(meta, links) {
+        let pagination = {
+          current_page: meta.current_page,
+          last_page: meta.last_page,
+          next_page_url: links.next,
+          prev_page_url: links.prev
+        };
+
+        this.pagination = pagination;
+      },
+      deleteArticle(id) {
+        if(confirm('Are You Sure?')){
+          fetch(`api/article/${id}`,{
+            method: 'delete'
+          })
+          .then(res => res.json())
+          .then(data => {
+            alert('Article Remove');
+            this.fetchArticles();
+          })
+          .catch(err => console.log(err));
+        }
+      },
+      addArticle() {
+        if (this.edit === false) {
+          //add
+          fetch('api/article', {
+            method: 'post',
+            body: JSON.stringify(this.article),
+            headers: {
+              'content-type': 'application/json'
+            }
+          })
+          .then(res => res.json())
+          .then(data => {
+            this.article.title = '';
+            this.article.body = '';
+            alert('Article Added');
+            this.fetchArticles();
+          })
+          .catch(err => console.log(err));
+        } else {
+          //update
+          fetch('api/article', {
+            method: 'put',
+            body: JSON.stringify(this.article),
+            headers: {
+              'content-type': 'application/json'
+            }
+          })
+          .then(res => res.json())
+          .then(data => {
+            this.article.title = '';
+            this.article.body = '';
+            alert('Article Updated');
+            this.fetchArticles();
+          })
+          .catch(err => console.log(err));
+        }
+      },
+      editArticle(article) {
+        this.edit = true;
+        this.article.id = article.id;
+        this.article.article_id = article.id;
+        this.article.title = article.title;
+        this.article.body = article.body;
+
+      }
+    }   
+ };
+      
+</script> 
